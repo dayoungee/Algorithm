a103b76d188a60ef90b321fb7f62597f0da21c94
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-01-09 13:50:05 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -7,28 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         solution(n);
     }
 
     static public void solution(int n){
         int answer = 0;
-        // do while 문을 할 수도 있지만 최초의 한번 일의자리 예외처리를 위해 빼줌
-        int ten = (n  % 10) * 10;
-        int cal = (n < 10) ? (n * 10) : n;
-        int unit = ((cal % 10) + (cal/10)) % 10;
-        cal = ten + unit;
-        answer++;
-
-        while(n != cal){
+        int original = n;
+        do{
             // 가독성 때문에 나눔, ten: 십의자리 연산 unit 일의자리 연산
-            ten = (cal % 10 ) * 10;
-            unit = ((cal % 10) + (cal/10)) % 10;
-            cal = ten + unit;
+            int ten = (n % 10 ) * 10;
+            int unit = ((n % 10) + (n/10)) % 10;
+            n = ten + unit;
             answer++;
 
-        }
+        }while(n != original);
+
         System.out.println(answer);
     }
-
 }
