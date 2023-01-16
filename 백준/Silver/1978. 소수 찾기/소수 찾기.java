0fd3f7fee211a56f9dbf164789386fe59279b7ba
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-01-16 14:16:38 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -4,41 +4,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int max = Integer.MIN_VALUE;
+class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 배열 넣기
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(arr[i], max);
-        }
-        System.out.println(solution(arr));
-    }
-    static public int solution(int[] arr){
         int answer = 0;
-        boolean[] primeCheck = new boolean[max+1];
-        // 예외처리
-        if(max < 2) return answer;
-        primeCheck[1] = true;
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(isPrime(num)) answer ++;
+        }
+        System.out.println(answer);
+    }
 
-        for(int i = 2; i <= Math.sqrt(max); i++){
-            if(primeCheck[i] == false){
-                for(int j = i*i; j <= max; j= j+i){
-                    primeCheck[j] = true;
-                }
+    static boolean isPrime(int num) {
+        if(num == 1) return false;
+        for(int i=2; i<num; i++) {
+            if(num % i == 0) {
+                return false;
             }
         }
-
-        for(int n : arr){
-            if(primeCheck[n] == false) answer++;
-        }
-        return answer;
+        return true;
     }
 
 }
