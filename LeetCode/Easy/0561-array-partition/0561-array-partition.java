4ac9419113b156319ea76d4e3c4edaf68bbf8e82
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-01-17 09:39:33 +0900

    Time: 13 ms (87.77%), Space: 44.5 MB (73.07%) - LeetHub

--- LeetCode/Easy/0561-array-partition/0561-array-partition.java
+++ LeetCode/Easy/0561-array-partition/0561-array-partition.java
@@ -2,8 +2,7 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         int answer = 0;
         Arrays.sort(nums);
-        int i = (nums.length % 2 == 0) ? 0 : 1;
-        for(;i < nums.length; i=i+2){
+        for(int i = 0;i < nums.length; i=i+2){
             answer += nums[i];
         }
         return answer;
