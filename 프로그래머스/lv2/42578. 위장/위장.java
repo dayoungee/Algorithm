b7725b094c50e483abaa81fd8d1979ea01ed96f8
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-01-17 15:52:41 +0900

    [level 2] Title: 위장, Time: 0.06 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int solution(String[][] clothes) {
-        int answer = 0;
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for(String[] arr : clothes){
+            map.put(arr[1], map.getOrDefault(arr[1], 0)+1);
+        }
+        for(String key : map.keySet()) {
+            answer *=(map.get(key)+1);
+        }
+        answer -=1;
         return answer;
     }
 }
