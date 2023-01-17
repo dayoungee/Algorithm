fd6f021f4e25460cb323614ceff28c25beb78bc0
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-01-17 11:45:44 +0900

    [Silver II] Title: 베르트랑 공준, Time: 180 ms, Memory: 15192 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,35 +1,40 @@
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Main {
-
+    static boolean check[] = new boolean[246913];
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
+        checkPrime();
+        
         while(true){
             int n = Integer.parseInt(br.readLine());
+            
             if(n == 0) break;
-            sb.append(solution(n)).append("\n");
+            
+            int answer = 0;
+            for(int i = n + 1; i <= n * 2; i++){
+                if(!check[i]) answer++;
+            }
+            sb.append(answer).append("\n");
         }
+        
         System.out.print(sb);
     }
 
-    public static int solution(int n){
-        int answer = 0;
-        for(int i = n + 1; i <= n*2; i++){
-            boolean isPrime = true;
-            for(int j = 2; j <= Math.sqrt(i); j++){
-                if(i % j == 0){
-                    isPrime = false;
-                    break;
+    private static void checkPrime() {
+        for(int i = 2; i <= Math.sqrt(check.length); i++){
+            if(check[i] == false){
+                for(int j = i*i; j <= check.length; j= j+i){
+                    check[j] = true;
                 }
             }
-            if(isPrime) answer++;
         }
-
-        return answer;
     }
-    
 }
