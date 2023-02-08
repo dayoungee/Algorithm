76a122ec21316bc84b986ead4268539653b3ad1b
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-02-09 00:32:15 +0900

    [Silver II] Title: 에디터, Time: 544 ms, Memory: 73556 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,51 +1,45 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args)  throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        Stack stackL = new Stack<>();
-        Stack stackR = new Stack<>();
+        Stack<Character> stackL = new Stack<>();
+        Stack<Character> stackR = new Stack<>();
         String str = br.readLine();
         int n = Integer.parseInt(br.readLine());
         for(int i = 0; i < str.length();i++){
             stackL.push(str.charAt(i));
         }
-
         for (int i=0; i<n; i++) {
             String command = br.readLine();
             char c = command.charAt(0);
+            switch(c){
+                case 'L':
+                    if(!stackL.isEmpty()) stackR.push(stackL.pop());
 
-            if (c == 'L' && !stackL.isEmpty()) {
-                //왼쪽으로 이동시 하나 오른쪽으로 이동
-                stackR.push(stackL.pop());
-            } else if (c == 'D' && !stackR.isEmpty()) {
-                //오른쪽으로 이동시 하나 왼쪽으로 이동
-                stackL.push(stackR.pop());
-            } else if (c == 'B' && !stackL.isEmpty()) {
-                //삭제
-                stackL.pop();
-            } else if (c == 'P') {
-                //추가
-                stackL.push(String.valueOf(command.charAt(2)));
+                    break;
+                case 'D':
+                    if(!stackR.isEmpty()) stackL.push(stackR.pop());
+
+                    break;
+                case 'B':
+                    if(!stackL.isEmpty()) stackL.pop();
+                    break;
+                case 'P':
+                    stackL.push(command.charAt(2));
+                    break;
             }
         }
 
-        while (!stackL.isEmpty()) {
-            stackR.push(stackL.pop());
-        }
-        while (!stackR.isEmpty()) {
-            sb.append(stackR.pop());
-        }
+        while (!stackL.isEmpty()) stackR.push(stackL.pop());
+
+        while (!stackR.isEmpty()) sb.append(stackR.pop());
 
         System.out.print(sb);
-
     }
 }
