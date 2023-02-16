355724df9c74df0d9e0f78909ab3c9ddb3e5afa6
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-02-16 19:16:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 448 ms, Memory: 293780 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,32 +1,34 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int i = 1; i < n+1; i++) queue.add(i);
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < n; i++) queue.add(i+1);
+        int cnt = 0;
         sb.append("<");
-        while(queue.size() != 1){
-            for(int i = 0; i < m-1; i++){
-                int tmp = queue.poll();
-                queue.add(tmp);
+        while(!queue.isEmpty()){
+            cnt++;
+            int num = queue.poll();
+            if(cnt == m){
+                sb.append(num);
+                if(!queue.isEmpty()) sb.append(", ");
+                cnt = 0;
             }
-            sb.append(queue.poll()).append(", ");
+            else queue.add(num);
         }
-        sb.append(queue.poll()).append(">");
+        sb.append(">");
         System.out.print(sb);
-
     }
 }
