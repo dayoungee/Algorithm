b4edffbbc96cae3f5f0fb855490f7895a72e1dd6
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-02-16 12:24:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)br.readLine().charAt(0));
+    }
+
+}
