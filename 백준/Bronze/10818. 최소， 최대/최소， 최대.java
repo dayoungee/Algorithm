6ae12cdc21ddb8f13da526cfb0b2d7d937846825
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-02-19 00:59:44 +0900

    [Bronze III] Title: 최소, 최대, Time: 492 ms, Memory: 88416 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.OptionalInt;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(n --> 0){
-            arr.add(Integer.parseInt(st.nextToken()));
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        while(n-->0){
+            int m = Integer.parseInt(st.nextToken());
+            max = Math.max(m, max);
+            min = Math.min(m, min);
         }
-        // mapToInt: Stream에서 Int 원시 스트림으로 변환한다.
-        // orElseThrow(): 과가 없는 경우에는 NoSuchElementException 예외를 throw한다
-        int max = arr.stream().mapToInt(x->x).max().orElseThrow(NoSuchElementException::new);
-        int min = arr.stream().mapToInt(x->x).min().orElseThrow(NoSuchElementException::new);
+        System.out.println(min + " "+ max);
 
-        System.out.println(min + " " + max);
     }
 
 }
