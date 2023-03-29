f0a301227b3287de51fca34de6ed158a94e328de
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-03-29 20:49:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 18868 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,53 +1,43 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class Main {
-    static int N;
-    static int K;
-    static int[] check = new int[100001];
+    static int[] distance = new int[100001];
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        if (N == K) {
-            System.out.println(0);
-        } else {
-            bfs(N);
-        }
+        BFS(n, m);
+        System.out.print(distance[m]);
+
     }
 
-    static void bfs(int num) {
+    private static void BFS(int n, int m) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(num);
-        check[num] = 1;
-
-        while (!q.isEmpty()) {
+        distance[n] = 0;
+        q.add(n);
+        if(n == m) return;
+        while(!q.isEmpty()){
             int temp = q.poll();
-
-            for (int i = 0; i < 3; i++) {
+            for(int i = 0; i < 3; i++){
                 int next;
-
-                if (i == 0) {
-                    next = temp + 1;
-                } else if (i == 1) {
-                    next = temp - 1;
-                } else {
-                    next = temp * 2;
-                }
-
-                if (next == K) {
-                    System.out.println(check[temp]);
-                    return;
-                }
-
-                if (next >= 0 && next < check.length && check[next] == 0) {
-                    q.add(next);
-                    check[next] = check[temp] + 1;
-                }
+                if(i == 0)      next = temp + 1;
+                else if(i == 1) next = temp - 1;
+                else            next = temp * 2;
+                if(next < 0 || next >= distance.length || distance[next] != 0) continue;
+                q.add(next);
+                distance[next] = distance[temp] + 1;
+                if(next == m) return;
             }
         }
     }
+
 }
