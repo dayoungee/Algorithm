1d59dee92e0a149f49b7636b88d10dd16d754b5d
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-04-18 22:12:39 +0900

    [Silver II] Title: N과 M (11), Time: 400 ms, Memory: 34304 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -7,33 +7,39 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    static Set<String> set = new LinkedHashSet<>();
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+
         for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
         Arrays.sort(arr);
+
         permutation(arr, new int[m], 0, m);
-        for(String s : set){
-            System.out.print(s);
-        }
+
+        System.out.println(sb);
     }
 
     private static void permutation(int[] arr, int[] out, int depth, int r) {
         if(depth == r){
-            StringBuilder sb = new StringBuilder();
             for(int n : out) sb.append(n).append(" ");
             sb.append("\n");
-            set.add(sb.toString());
             return;
         }
+        int before = 0;
         for(int i = 0; i < arr.length; i++){
-            out[depth] = arr[i];
-            permutation(arr, out, depth+1, r);
+            if(before != arr[i]){
+                out[depth] = arr[i];
+                permutation(arr, out, depth+1, r);
+                before = arr[i];
+            }
         }
     }
 
