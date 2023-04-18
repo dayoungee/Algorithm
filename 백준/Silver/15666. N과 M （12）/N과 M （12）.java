5b46c6dad847abb3988ce0c22c8ebedd29562683
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-04-18 22:23:51 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        combination(arr, new int[m], 0, 0, m);
+
+        System.out.println(sb);
+    }
+
+    private static void combination(int[] arr, int[] out, int start, int depth, int r) {
+        if(depth == r){
+            for(int n : out) sb.append(n).append(" ");
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+        for(int i = start; i < arr.length; i++){
+            if(before != arr[i]){
+                out[depth] = arr[i];
+                before = arr[i];
+                combination(arr, out, i, depth + 1, r);
+            }
+        }
+    }
+
+
+}
