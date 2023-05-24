c2628da9972a0fa9217cba1ef4084e1a90034e8c
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-05-24 19:16:20 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,4 +1,4 @@
-SELECT date_format(sales_date, '%Y-%m-%d') sales_date, product_id, user_id, sales_amount
+/*SELECT date_format(sales_date, '%Y-%m-%d') sales_date, product_id, user_id, sales_amount
 from online_sale
 where date_format(sales_date, '%Y-%m') = '2022-03'
 union all
@@ -6,4 +6,13 @@ select date_format(sales_date, '%Y-%m-%d')sales_date, product_id, NULL as user_i
 from offline_sale 
 where date_format(sales_date, '%Y-%m') = '2022-03'
 
-order by sales_date, product_id, user_id
+order by sales_date, product_id, user_id*/
+
+SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+FROM ONLINE_SALE
+WHERE SALES_DATE LIKE '%-03-%'
+UNION ALL
+SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT
+FROM OFFLINE_SALE
+WHERE SALES_DATE LIKE '%-03-%'
+ORDER BY sales_date, product_id, user_id ;
