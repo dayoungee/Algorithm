7f551f257df56b079e4d1eeef5dc2940f5db6790
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-08-18 19:42:22 +0900

    [level 2] Title: 소수 찾기, Time: 82.00 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,33 +1,31 @@
 import java.util.*;
 
 class Solution {
+    boolean[] check = new boolean[10000000];
     public int solution(String numbers) {
-        Set<Integer> set = new HashSet<>();
-        // 모든 조합
-        combination(numbers, "", set);
-        // 소수 판별
         int answer = 0;
-        for (Integer num : set) {
-            int i;
-            for (i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    break;
-                }
-            }
-            if (i == num) {
-                answer++;
-            }
+        Set<Integer> set = new HashSet<>();
+        setPrime();
+        combination(numbers, "", set);
+        for(Integer n : set){
+            if(!check[n] && n > 1) answer++;
         }
         return answer;
     }
-
-    public void combination(String numbers, String str, Set<Integer> set) {
+    public void combination(String numbers, String str, Set<Integer> set){
         int len = numbers.length();
         if (!"".equals(str)) {
             set.add(Integer.valueOf(str));
         }
         for (int i = 0; i < len; i++) {
             combination(numbers.substring(0, i) + numbers.substring(i + 1, len), str + numbers.charAt(i), set);
+        }
+    }
+    public void setPrime(){
+        for(int i = 2; i <= Math.sqrt(check.length); i++){
+            if(!check[i]){
+                for(int j = i * i; j <= check.length -1; j += i) check[j] = true;
+            }
         }
     }
     
