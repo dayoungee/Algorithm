271b8592b541afba00bb8b963a1b61c06aab9388
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-08-22 15:33:04 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.08 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long n) {
+        int len = (int)Math.log10(n) + 1;
+        int[] answer = new int[len];
+        String str = String.valueOf(n);
+        for(char c : str.toCharArray()){
+            answer[--len] = (int)c - '0';
+        }        
+        return answer;
+    }
+}
