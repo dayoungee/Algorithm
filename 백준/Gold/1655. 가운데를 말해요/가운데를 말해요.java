310fff48e60f244b9ba0025ebe0ef85bfb4d8038
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-10-19 22:52:12 +0900

    [Gold II] Title: 가운데를 말해요, Time: 488 ms, Memory: 33760 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,31 +1,30 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		
-		Queue<Integer> maxHeap = new PriorityQueue<>((o1,o2) -> o2-o1); // 내림차순  
-		Queue<Integer> minHeap = new PriorityQueue<>((o1,o2) -> o1-o2); // 오름차순 
-		
-		for(int i=0; i<n; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(maxHeap.size() == minHeap.size()) maxHeap.add(num);
-			else minHeap.add(num);
-            
-			// maxHeap이 더 클 경우 원소 switch
-			if(!maxHeap.isEmpty() && !minHeap.isEmpty()) {
-				if(maxHeap.peek() > minHeap.peek()) {
-					int tmp = maxHeap.poll();
-					maxHeap.offer(minHeap.poll());
-					minHeap.offer(tmp);
-				}
-			}
-			sb.append(maxHeap.peek()+"\n");
-		}
-		System.out.println(sb.toString());
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(maxPq.size() == minPq.size()) maxPq.add(num);
+            else minPq.add(num);
+
+            if(!minPq.isEmpty() && !maxPq.isEmpty())
+                if(minPq.peek() < maxPq.peek()){
+                    int tmp = maxPq.poll();
+                    maxPq.add(minPq.poll());
+                    minPq.add(tmp);
+                }
+
+            sb.append(maxPq.peek()).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
