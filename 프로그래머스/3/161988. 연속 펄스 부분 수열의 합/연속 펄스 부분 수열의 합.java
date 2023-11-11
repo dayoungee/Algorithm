3d5a884f9129ed832401cf34ce771c19afacf2ef
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-11-12 01:08:59 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 23.03 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,15 +1,21 @@
 class Solution {
     public long solution(int[] sequence) {
-        long answer = Integer.MIN_VALUE;
-        long[] dp = new long[sequence.length + 1];
-        long max = 0, min = 0;
-        for(int i = 0; i < sequence.length; i++) {
-            if(i % 2 == 1) sequence[i] *= -1;
-            dp[i+1] = dp[i] + sequence[i];
-            max = Math.max(dp[i+1], max);
-            min = Math.min(dp[i+1], min);
+        long[] dp1 = new long[sequence.length];
+        long[] dp2 = new long[sequence.length];
+        dp1[0] = sequence[0];
+        dp2[0] = sequence[0] * -1;
+        long answer = Math.max(dp1[0], dp2[0]);
+        for(int i = 1; i < sequence.length; i++) {
+            if(i % 2 == 0) {
+                dp1[i] = Math.max(dp1[i-1] + sequence[i], sequence[i]);
+                dp2[i] = Math.max(dp2[i-1] + sequence[i] * -1, sequence[i] * -1);
+            } else {
+                dp1[i] = Math.max(dp1[i-1] + sequence[i] * -1, sequence[i] * -1);
+                dp2[i] = Math.max(dp2[i-1] + sequence[i], sequence[i]);
+            } 
+            answer = Math.max(answer, Math.max(dp1[i], dp2[i]));
         }
         
-        return max - min;
+        return answer;
     }
 }
