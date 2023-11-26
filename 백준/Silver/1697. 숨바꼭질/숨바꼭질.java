b3361f7e7156b8db4154967183b0273d4ab5dbaa
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-11-26 20:05:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 18844 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,43 +1,36 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] distance = new int[100001];
-    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        Queue<Integer> q = new LinkedList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        BFS(n, m);
-        System.out.print(distance[m]);
-
-    }
-
-    private static void BFS(int n, int m) {
-        Queue<Integer> q = new LinkedList<>();
-        distance[n] = 0;
         q.add(n);
-        if(n == m) return;
-        while(!q.isEmpty()){
+        int[] dist = new int[100_001];
+        while(!q.isEmpty()) {
             int temp = q.poll();
-            for(int i = 0; i < 3; i++){
+            if(temp == m) {
+                System.out.println(dist[temp]);
+                break;
+            }
+            for(int i = 0; i < 3; i++) {
                 int next;
-                if(i == 0)      next = temp + 1;
+                if(i == 0) next = temp + 1;
                 else if(i == 1) next = temp - 1;
-                else            next = temp * 2;
-                if(next < 0 || next >= distance.length || distance[next] != 0) continue;
+                else next = temp * 2;
+                if(next < 0 || next > 100_000 || dist[next] != 0) continue;
                 q.add(next);
-                distance[next] = distance[temp] + 1;
-                if(next == m) return;
+                dist[next] = dist[temp] + 1;
             }
         }
     }
-
 }
